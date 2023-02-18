682d5849498170616baaf0f87d7884735673743d
Ifeoluwa Seriki <ifeoluwaalice75@gmail.com>
2023-02-17 20:04:58 -0800

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 
-/**
- *main - print a num pair from 00-99 but no repeats (00 01, 00 02, 00 03,...)
- *Return: Always 0 (Success)
- */
-
 int main(void)
 {
-	int tens;
-	int ones;
-	int t;
-	int o;
+    int tens;
+    int ones;
+    int t;
+    int o;
+    int first_pair = 1;
 
-	for (tens = '0'; tens <= '9'; tens++) /*print first two digit combo*/
-	{
-		for (ones = '0'; ones <= '9'; ones++)
-		{
-		 	for (t = tens; t <= '9'; t++) /*print second of pair*/
-			{
-				for (o = ones + 1; o <= '9'; o++)
-				{
-					putchar(tens);
-					putchar(ones);
-					putchar(' ');
-					putchar(t);
-					putchar(o);
+    for (tens = 0; tens < 100; tens++) {
+        for (ones = tens + 1; ones < 100; ones++) {
+            t = ones % 10;
+            o = tens % 10;
+            tens /= 10;
+            ones /= 10;
+            putchar('0' + tens);
+            putchar('0' + ones);
+            putchar(' ');
+            putchar('0' + o);
+            putchar('0' + t);
 
-					if (!((tens == '9' && ones == '8') &&
-					      (t == '9' && o == '9')))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-				o = '0';
-			}
-		}
-	}
-	putchar('\n');
-
-	return (0);
+            if (first_pair) {
+                first_pair = 0;
+            } else {
+                putchar(',');
+                putchar(' ');
+            }
+            tens *= 10;
+            ones *= 10;
+            tens += o;
+            ones += t;
+        }
+    }
+    putchar('\n');
+    return 0;
 }
