ecbc6339055bad6dfe0d0fc2a6ff921c4a7c716e
Ifeoluwa Seriki <ifeoluwaalice75@gmail.com>
2023-02-21 17:42:39 -0800

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit of integer
+ * @n: integer argument
+ * Return: last digit of number
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,10 @@
+# Functions and nested loops
+
+### Authors
+
+- ifemi
+
+### Requirements
+
+- C programming language
+- Ubuntu 20.04 LTS
