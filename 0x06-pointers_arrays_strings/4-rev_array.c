d2cc5c60a1166cf5e29126d0af3373868b41dd68
Ifeoluwa Seriki <ifeoluwaalice75@gmail.com>
2023-03-01 17:33:13 -0800

    Strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * _strcmp - compares two strings.
- * @s1: pointer to first string.
- * @s2: pointer to second string.
- * Return: value less than 0 if string is less than the other.
- * value greater than 0 if string is greater than the other.
- * 0 if strings are equal.
+ * reverse_array - revereses an array.
+ * @a: pointer to array.
+ * @n: number of elements of an array.
+ *
+ * Return: void.
  */
-int _strcmp(char *s1, char *s2)
+void reverse_array(int *a, int n)
 {
-	int counter, cmpVal;
+	int temp, counter;
 
+	n = n - 1;
 	counter = 0;
-	while (s1[counter] == s2[counter] && s1[counter] != '\0')
+	while (counter <= n)
 	{
-		counter++;
+		temp = a[counter];
+		a[counter++] = a[n];
+		a[n--] = temp;
 	}
-	cmpVal = s1[counter] - s2[counter];
-	return (cmpVal);
 }
