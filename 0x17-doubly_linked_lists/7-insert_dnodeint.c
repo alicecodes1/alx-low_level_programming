306f290a2099a3bbf35413b3bc50fd7d516ae6d8
Seriki Ifeoluwa <ifeoluwaalice75@gmail.com>
2023-05-18 16:29:03 -0700

    0x17-doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_node -to insert node at given index
+ * @tmp: pointer to the nth position node in doubly linked list
+ * @n: node data
+ * Return: address of inserted node
+ */
+dlistint_t *insert_node(dlistint_t *tmp, int n)
+{
+	dlistint_t *new;
+
+	new = malloc(sizeof(struct dlistint_s));
+	if (!new)
+		return (NULL);
+	new->n = n;
+
+	new->next = tmp;
+	new->prev = tmp->prev;
+	tmp->prev->next = new;
+	tmp->prev = new;
+
+	return (new);
+}
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+
+	dlistint_t *tmp;
+
+	/* to insert at beginning of the  empty or existing linked list */
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+	if (!h)
+		return (NULL)
+
+	tmp = *h;
+	while ((idx != 0) && (tmp->next))
+	{
+		idx -= 1;
+		tmp = tmp->next;
+		if (idx == 0)
+			return (insert_node(tmp, n));
+	}
+
+	if (idx == 1)
+		return (add_dnodeint_end(h, n));
+	return (NULL);
+}
